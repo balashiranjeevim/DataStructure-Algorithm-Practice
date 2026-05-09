d886838912ff4bcea8c6fadc72f92e3ee1a0935a
BALA SHIRANJEEVI <balashiranjeevi97@gmail.com>
2026-05-09 08:37:03 +0530

    Creted Array Folder and Added Easy sub folder then Written a code for Finding Largest Element in the array

---
+++ Arrays/Easy/LargestElement.java
@@ -0,0 +1,24 @@
+package Arrays.Easy;
+
+public class LargestElement {
+
+    public static void main(String[] args){
+
+        int[] array = {10,99,100,1501,6,28,55,60,40,90,10,7};
+
+        System.out.println(largestHelper(array, array.length-1, 0));
+    }
+
+    public static int largestHelper(int[] array, int size, int element){
+        if(size <0){
+            return element;
+        }
+        
+        if(array[size] > element){
+            element = array[size];
+        }
+        
+        return largestHelper(array, size-1, element);
+        
+    }
+}
