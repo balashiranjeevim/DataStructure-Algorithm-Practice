a7de64f1a1fa7b46759f04c6c443483f88ab400e
balashiranjeevim <balashiranjeevim@gmail.com>
2026-05-11 08:21:34 +0530

    Solved and Updated the code for Arrays/Easy/SortedArrayCheck.java

---
+++ Arrays/Easy/SortedArrayCheck.java
@@ -0,0 +1,32 @@
+/*
+
+Given an array arr[], check whether it is sorted in non-decreasing order. Return true if it is sorted otherwise false.
+
+*/
+
+package Arrays.Easy;
+
+import java.util.Scanner;
+
+public class SortedArrayCheck {
+
+    public static void main(String[] args) {
+
+        int[] array = { 10, 10, 80, 100, 150, 200 };
+
+        System.out.println(isSorted(array));
+    }
+
+    public static boolean isSorted(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
