9941bdc1dbf645cf6ae0819fb5fc9a5bde2cf9d0
balashiranjeevim <balashiranjeevim@gmail.com>
2026-05-16 19:24:02 +0530

    Updated Better Solution for Arrays/Easy/RotateArray.java

--- Arrays/Easy/RotateArray.java
+++ Arrays/Easy/RotateArray.java
@@ -21,37 +21,68 @@ Input: nums = [1, 2, 3, 4, 5], n = 2
 Output: [3, 4, 5, 1, 2]
 */
 
-
 package Arrays.Easy;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class RotateArray {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int[] array = {1, 2, 3, 4, 5, 6};
+        int k = input.nextInt();
+        int[] array = { 1, 2, 3, 4, 5, 6 };
 
         System.out.println("Before : " + Arrays.toString(array));
 
-        leftRotate_Array(array, n);
+        leftRotate_Array(array, k);
 
         System.out.println("After : " + Arrays.toString(array));
     }
 
-    public static void leftRotate_Array(int[] array, int operation){
-        while(operation > 0){
-            int pointer = 0;
-            int temp = array[0];
+    public static void leftRotate_Array(int[] array, int k) {
 
-            while(pointer < array.length-1 ){
-                array[pointer] = array[pointer+1];
-                pointer++;
-            }
-            array[pointer] = temp;
-            operation--;
-            
+        // Reverse first k elements:
+        reverseMethod(array, 0, k - 1);
+
+        // Reverse remaining:
+        reverseMethod(array, k, array.length - 1);
+
+        // Reverse whole array:
+        reverseMethod(array, 0, array.length - 1);
+
+    }
+
+    public static void reverseMethod(int[] array, int left, int right) {
+        while (left < right) {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
         }
     }
 }
+
+/*
+ * 
+ * The best approach is the Reversal Algorithm because,
+ * 
+ * O(n) Time
+ * O(1) Extra Space
+ * 
+ * Instead of shifting elements one by one, we reverse parts of the array
+ * 
+ * 
+ * Original:
+ * [1,2,3,4,5,6]
+ * 
+ * Reverse first k elements:
+ * [2,1,3,4,5,6]
+ * 
+ * Reverse remaining:
+ * [2,1,6,5,4,3]
+ * 
+ * Reverse whole array:
+ * [3,4,5,6,1,2]
+ * 
+ */
